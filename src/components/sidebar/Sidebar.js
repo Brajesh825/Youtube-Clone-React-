a6cbb6f096f1e75196970d3f1b5f6b012ef23a80
brajeshmishra825 <brajeshmishra825@gmail.com>
2022-06-10 16:09:15 +0530

    Sidebar done

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,10 +1,55 @@
-import React from 'react'
-import "./_sidebar.scss"
+import React from "react";
+import "./_sidebar.scss";
+
+import {
+  MdSubscriptions,
+  MdExitToApp,
+  MdThumbUp,
+  MdHistory,
+  MdLibraryBooks,
+  MdHome,
+  MdSentimentDissatisfied,
+} from "react-icons/md";
 
 const Sidebar = () => {
   return (
-    <div className='border border-danger' >Sidebar</div>
-  )
-}
+    <div className="border border-danger sidebar">
+      <li>
+        <MdHome size={23} />
+        <span>Home</span>
+      </li>
+      <li>
+        <MdSubscriptions size={23} />
+        <span>Subscriptions</span>
+      </li>
 
-export default Sidebar
+      <li>
+        <MdThumbUp size={23} />
+        <span>Liked Video</span>
+      </li>
+
+      <li>
+        <MdHistory size={23} />
+        <span>History</span>
+      </li>
+
+      <li>
+        <MdLibraryBooks size={23} />
+        <span>Library</span>
+      </li>
+      <li>
+        <MdSentimentDissatisfied size={23} />
+        <span>I don't Know</span>
+      </li>
+
+      <hr />
+
+      <li>
+        <MdExitToApp size={23} />
+        <span>Log Out</span>
+      </li>
+    </div>
+  );
+};
+
+export default Sidebar;
